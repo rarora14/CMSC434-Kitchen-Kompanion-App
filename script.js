497324acaca812cf0c88f082ab32eb351e121d92
Luke Stadler <lukestads@gmail.com>
2025-10-02 12:22:50 -0400

    comments

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/tab_role
 function openTab(tabId) {
-  // Remove active from all tabs and buttons
+  //Remove active from all tabs and buttons
   document.querySelectorAll('.tab-content').forEach((el, idx) => {
     el.classList.remove('active');
     if (document.getElementById('tabBtn' + (idx + 1))) {
       document.getElementById('tabBtn' + (idx + 1)).classList.remove('active');
     }
   });
-  // Add active to selected tab and button
+  //Add active to selected tab and button
   const tabIdx = parseInt(tabId.replace('tab', ''));
   document.getElementById(tabId).classList.add('active');
   if (document.getElementById('tabBtn' + tabIdx)) {
@@ -17,13 +17,14 @@ function openTab(tabId) {
 }
 
 //https://www.w3schools.com/howto/howto_css_modals.asp
+//Close profile modal
 function closeProfileAlert() {
   document.getElementById('profileModal').classList.add('hidden');
 }
 
+//Listens to clicks on profile pic to open modal
 document.addEventListener('DOMContentLoaded', () => {
   openTab('tab1');
-  // Profile image click
   const profileImg = document.getElementById('profileImg');
   if (profileImg) {
     profileImg.onclick = function() {
@@ -32,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+//show selected choices
 function showChoice() {
   const color = document.querySelector('input[name="color"]:checked');
   const fruit = document.getElementById('fruitSelect');
@@ -42,19 +44,57 @@ function showChoice() {
 }
 
 //https://www.w3schools.com/howto/howto_js_todolist.asp
-function addTodo() {
-  const input = document.getElementById('todoInput');
-  const list = document.getElementById('todoList');
-  const value = input.value.trim();
-  if (!value) return;
-  const li = document.createElement('li');
-  li.textContent = value;
-  li.onclick = function() {
-    li.classList.toggle('done');
-    if (li.classList.contains('done')) {
-      setTimeout(() => { if (li.parentNode) li.parentNode.removeChild(li); }, 500);
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-  };
-  list.appendChild(li);
-  input.value = '';
+  }
 }
